ebf14a967183e4b7f986e5b33642463e46abcf6b
anup0453 <101499158+anup0453@users.noreply.github.com>
2023-02-20 11:03:33 +0530

    AND OR NOT

---
+++ boolean.sh
@@ -0,0 +1,27 @@
+# AND (-a) (&&)
+# OR (-o) ||
+# NOT (-n) !
+
+echo -e "enter  the first number"
+read -r a
+
+echo -e "enter  the second number"
+read -r b
+
+echo -e "enter  the third number"
+read -r c
+
+
+if test $a -gt 35 -a $b -ge 35 -a $c -ge 35
+then
+echo "You have qualified !!!"
+else
+echo "Please try next time."
+fi
+
+if (( $a > 35 && $b > 35 && $c > 35 ));
+then
+echo "Congrats"
+else
+echo "sorry"
+fi
